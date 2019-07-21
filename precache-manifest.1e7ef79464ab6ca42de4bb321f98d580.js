af3a76515bad627c4318969a627b96c93a21e333
chiheb <chiheb.rabeh@gmail.com>
2019-07-21 15:10:43 +0100

    Updates

---
+++ precache-manifest.1e7ef79464ab6ca42de4bb321f98d580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e059741f080b0cbac6f732dcd8cef8f",
+    "url": "/basic-redux-complex-credit-card/./index.html"
+  },
+  {
+    "revision": "9940a5ffacfe430c3ecd",
+    "url": "/basic-redux-complex-credit-card/./static/css/main.254e1529.chunk.css"
+  },
+  {
+    "revision": "821d3e64fa285ca636d1",
+    "url": "/basic-redux-complex-credit-card/./static/js/2.d8720ea2.chunk.js"
+  },
+  {
+    "revision": "9940a5ffacfe430c3ecd",
+    "url": "/basic-redux-complex-credit-card/./static/js/main.674e6adc.chunk.js"
+  },
+  {
+    "revision": "bafb1403ecdc23fcde1e",
+    "url": "/basic-redux-complex-credit-card/./static/js/runtime~main.1a991497.js"
+  },
+  {
+    "revision": "5f7f13078c28da8d960d8b3347335719",
+    "url": "/basic-redux-complex-credit-card/./static/media/slate_card.5f7f1307.png"
+  }
+]);
